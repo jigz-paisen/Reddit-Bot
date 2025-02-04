39b0b74c20b1461fa5beb49985ff08dce3963cbb
jigz-paisen <103289547+jigz-paisen@users.noreply.github.com>
2025-02-04 22:29:32 +0000

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ client_id""
 client_secret=""
 
 
-//details would be obtained from Reddit account sooooo ¯⁠\⁠_⁠(⁠ツ⁠)⁠_⁠/⁠¯use yours
+//details would be from your Reddit account sooooo ¯⁠\⁠_⁠(⁠ツ⁠)⁠_⁠/⁠¯use yours
